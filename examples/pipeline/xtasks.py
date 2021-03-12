ce6f1e17413aca0e45c4ede842403916ed55fb81
Jean-Claude <jc@wippler.nl>
2021-03-12 14:54:42 +0100

    cleanup

--- examples/pipeline/xtasks.py
+++
@@ -1,8 +0,0 @@
-# see https://www.pyinvoke.org
-from invoke import task
-
-@task(help={"args": "comma-separated list of commands"})
-def run(c, args):
-    """process commands as a pipeline"""
-    c.run("pio run -c ../pio-examples.ini -e native -s", env={"MONTY_VERSION": "pipeline"}, pty=True)
-    c.run(".pio/build/native/program %s" % args.replace(",", " "), pty=True)
