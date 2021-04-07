4f53506acd2554fbbf18560aa4ebed15e273d281
Jean-Claude <jc@wippler.nl>
2021-04-07 16:15:44 +0200

    add truly minimal blink demo, without Monty

---
+++ examples/justmcu/blink/README.md
@@ -0,0 +1,1 @@
+Minimal blinker for the new "mcu" hardware library.
---
+++ examples/justmcu/blink/main.cpp
@@ -0,0 +1,30 @@
+#include <mcu.h>
+
+using namespace mcu;
+
+// TODO the mcu::Pin struct clashes with the altpins::Pin function
+mcu::Pin leds [7];
+
+void mcu::failAt (void const* pc, void const* lr) {
+    while (true) {
+        leds[0].toggle(); // fast blink
+        for (uint32_t i = 0; i < systemClock() >> 8; ++i) {}
+    }
+}
+
+void mcu::idle () {
+    leds[1] = 0;
+    asm ("wfi");
+    leds[1] = 1;
+}
+
+int main () {
+    mcu::Pin::define("A6:P,A5:P,A4:P,A3:P,A1:P,A0:P,B3:P", leds, 7);
+
+    while (true) {
+        msWait(100);
+        auto t = millis();
+        for (int i = 2; i < 7; ++i)
+            leds[i] = t % i == 0;
+    }
+}
