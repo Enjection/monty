521378a3aa90543280f4804a0da2c9ec3f019cf4
Jean-Claude <jc@wippler.nl>
2021-04-08 23:18:43 +0200

    simplify and lower to 115200 baud for reliable comms

--- examples/f7/.gdbinit
+++ examples/f7/.gdbinit
@@ -1,6 +1,6 @@
 tar ext | openocd -f board/stm32f7discovery.cfg -c "gdb_port pipe"
 mon halt
-mon tpiu config internal :6464 uart false 200000000
+mon tpiu config internal :6464 uart false 200000000 115200
 file .pio/build/disco-f750/firmware.elf
 load
 run
--- examples/f7/main.cpp
+++ examples/f7/main.cpp
@@ -7,44 +7,20 @@ using namespace mcu;
 
 mcu::Pin led;
 
-namespace swo {
-    constexpr auto SWO_FREQ  = 2250000;
-    constexpr auto HCLK_FREQ = 200'000'000;
-
+static void swoPutc (int c) {
     constexpr auto ITM8 =  io8<0xE000'0000>;
     constexpr auto ITM  = io32<0xE000'0000>;
-    constexpr auto CDBG = io32<0xE000'EDF0>;
-    constexpr auto TPIU = io32<0xE004'0000>;
-
     enum { TER=0xE00, TCR=0xE80, LAR=0xFB0 };
-    enum { DEMCR=0x0C };
 
-    void init () {
-        TPIU(0x2004) = 0b10'0111;   // DBGMCU_CR
-        CDBG(DEMCR) = CDBG(0x0C) | (1<<24) | (1<<0); // TRCENA & VC_CORERESET
-
-        TPIU(0x0004) = 1;           // port size = 1 bit
-        TPIU(0x00F0) = 2;           // trace port protocol = Uart
-        TPIU(0x0010) = HCLK_FREQ / SWO_FREQ - 1;
-        TPIU(0x0304) = 0x100;       // turn off formatter (0x02 bit)
-
-        ITM(LAR) = 0xC5ACCE55;      // Unlock Lock Access Registers
-        ITM(TCR)[3] = 1;            // ITM_TCR_DWTENA_Msk
-        ITM(TER)[0] = 1;            // Port 0
-        ITM(TCR)[0] = 1;            // ITM_TCR_ITMENA_Msk
-    }
-
-    void putc (int c) {
-        if (ITM(TCR)[0] && ITM(TER)[0]) {
-            while (ITM(0)[0] == 0) {}
-            ITM8(0) = c;
-        }
+    if (ITM(TCR)[0] && ITM(TER)[0]) {
+        while (ITM(0)[0] == 0) {}
+        ITM8(0) = c;
     }
 }
 
 Printer debugf (nullptr, [](void*, uint8_t const* ptr, int len) {
     while (--len >= 0)
-        swo::putc(*ptr++);
+        swoPutc(*ptr++);
 });
 
 void mcu::failAt (void const* pc, void const* lr) {
@@ -55,7 +31,7 @@ void mcu::failAt (void const* pc, void const* lr) {
 
 int main () {
     fastClock();
-    led.define("K3:P"); // set K3 to output "0", turns off the LCD backlight
+    led.define("K3:P"); // set PK3 pin low to turn off the LCD backlight
 
     led.define("I1:P");
 
@@ -63,7 +39,7 @@ int main () {
     while (true) {
         //msWait(100);
         auto t = millis();
-debugf("hello %d\n", t);
+debugf("hello %s\n", "world");
         led = (t / 100) % 5 == 0;
     }
 }
