6efe2ab8c7b5f61d552e02057e9e2d221b63917d
Jean-Claude <jc@wippler.nl>
2021-06-16 13:15:22 +0200

    bring in more expansion code from codegen.py

--- scripts/gen.py
+++ scripts/gen.py
@@ -71,13 +71,97 @@ def traverse(tree, state):
                         node[-1][:] = r
     return state
 
-class Ops:
+class Expander:
+
     def ARGS(self, block, *args):
-        print(args, self.fname)
-        print(block[0])
-    def TYPE(self, block, *args):
-        print(len(block), ":", args, self.fname)
-        print(block[0])
+        '''generate positional arg checks and conversions'''
+        out, names, types, decls = [], [], "", {}
+        for a in args:
+            if a in ["?", "*"]:
+                types += a
+            else:
+                n, t = (a + ":v").split(":")[:2]
+                names.append(n)
+                types += t
+                if t not in decls:
+                    decls[t] = []
+                decls[t].append(n)
+        tmap = {"v":"Value", "i":"int", "o":"Object", "s":"char const"}
+        for t in decls:
+            m = tmap[t]
+            if t in "os":
+                out += ["%s *%s;" % (m, ", *".join(decls[t]))]
+            else:
+                out += ["%s %s;" % (m, ", ".join(decls[t]))]
+        params = ",&".join([""] + names)
+        out += ['auto ainfo = args.parse("%s"%s);' % ("".join(types), params),
+                "if (ainfo.isObj()) return ainfo;"]
+        return out
+
+    def BIND(self, block, fun, *args):
+        '''bind a function, i.e. define a callable function object wrapper'''
+        tmap = {"v":"Value", "i":"int", "o":"Object*", "s":"char const*"}
+        out, params, types = [], [], ""
+        for a in args:
+            if a in ["?", "*"]:
+                types += a
+            else:
+                n, t = (a + ":v").split(":")[:2]
+                params.append("%s %s" % (tmap[t], n))
+                types += t
+        if "?" in types or "*" in types:
+            params.insert(0, "ArgVec const& args")
+        return ['static auto f_%s (%s) -> Value {' % (fun, ", ".join(params))]
+
+    def KWARGS(self, block, *args):
+        '''generate option parsing code, i.e. keyword args'''
+        out = ["Value %s;" % ", ".join(args),
+            "for (int i = 0; i < args.kwNum(); ++i) {",
+            "    auto k = args.kwKey(i), v = args.kwVal(i);",
+            "    switch (k.asQid()) {"]
+        for a in args:
+            out.append('        case Q(0,"%s"): %s = v; break;' % (a, a))
+        out += ['        default: return {E::TypeError, "unknown option", k};',
+                "    }",
+                "}"]
+        return out
+
+    def OP(self, block, typ='', multi=0):
+        '''expand opcode functions'''
+        op = block[0].split()[1][2:]
+        if 'q' in typ:
+            fmt, arg, decl = ' %s', 'fetchQ()', 'Q arg'
+        elif 'v' in typ:
+            fmt, arg, decl = ' %u', 'fetchV()', 'int arg'
+        elif 'o' in typ:
+            fmt, arg, decl = ' %d', 'fetchO()', 'int arg'
+        elif 's' in typ:
+            fmt, arg, decl = ' %d', 'fetchO()-0x8000', 'int arg'
+        elif 'm' in typ:
+            fmt, arg, decl = ' %d', '_ip[-1]', 'uint32_t arg'
+        else:
+            fmt, arg, decl = '', '', ''
+        return ['void op%s (%s) {' % (op, decl)]
+
+    def TYPE(self, block, tag, *args):
+        '''generate the header of an exposed type'''
+        line = block[0].split()
+        name, base = line[1], line[line.index(":")+1:-1]
+        base = ' '.join(base) # accept multi-word, e.g. protected
+        out = [
+            block[0].strip(),
+            '    static auto create (ArgVec const&,Type const* =nullptr) -> Value;',
+            '    static Lookup const attrs;',
+            '    static Type info;',
+            '    auto type () const -> Type const& override { return info; }',
+            '    void repr (Buffer&) const override;',
+        ]
+        if tag.startswith('<'):
+            del out[1:3] # can't construct from the VM
+            del out[-1]  # no need for default repr override
+        #print(len(block), ":", args, self.fname)
+        #print(block[0])
+        return out
 
 class Stats:
     def __init__(self):
@@ -96,7 +180,7 @@ def main():
     print()
     traverse(tree, Stats()).dump()
     print()
-    traverse(tree, Ops())
+    traverse(tree, Expander())
 
 if __name__ == '__main__':
     main()
