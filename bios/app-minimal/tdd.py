9e1a7b50105522f2bc670d35df00fb26ac9155d8
Jean-Claude <jc@wippler.nl>
2021-07-01 15:21:53 +0200

    add much simpler tdd.py, use "make" to start it up

---
+++ bios/app-minimal/tdd.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+# Simple TDD watcher/runner - needs a matching makefile to work properly.
+# Everything is launched & configured via the makefile, launch as "make".
+# This runs on MacOS/Linux and requires "python3", "make", and "fswatch".
+# -jcw, May 2021
+
+import os, subprocess, sys, time
+
+# set up fswatch-based file-change watcher (MacOS & Linux), returns io stream
+def setupWatcher():
+    cmd = ['fswatch', '-l', '0.1']
+    if sys.platform == 'linux':
+        cmd += '-r --event Created --event Removed --event Updated'.split()
+    cmd += sys.argv[1:]
+    print("<<< %s >>>" % ' '.join(cmd))
+    subprocess.run(['make', 'run'])
+    return subprocess.Popen(cmd, stdout=subprocess.PIPE, text=True).stdout
+
+
+# process changes in Makefile, *.h, *.cpp, and this script
+def main():
+    last = 0
+    for line in setupWatcher():
+        fn = line.strip()
+        bn = os.path.basename(fn)
+        ext = os.path.splitext(bn)[1]
+        if bn == 'tdd.py':
+            print(bn, "changed")
+            sys.exit()
+
+        if ext in ['','.h','.cpp']:
+            if time.monotonic() > last + 0.5:
+                cmd = ['make', 'all']
+                if bn == 'Makefile':
+                    cmd.insert(1, 'clean')
+                subprocess.run(cmd)
+
+            last = time.monotonic()
+
+os.environ.pop('MAKELEVEL', None) # don't generate sub-make output in gmake
+
+verbose = False
+if sys.argv[1] == "-v":
+    verbose = True
+    del sys.argv[1]
+assert len(sys.argv) >= 2
+
+try:
+    main()
+except KeyboardInterrupt:
+    print()
+    sys.exit(2)
