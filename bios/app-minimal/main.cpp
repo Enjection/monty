a9f7c3cf5c76f0c80e73b2f73308e97b07cce26c
Jean-Claude <jc@wippler.nl>
2021-07-15 23:51:50 +0200

    add dimmable lcd backlight

--- bios/app-minimal/main.cpp
+++ bios/app-minimal/main.cpp
@@ -9,12 +9,14 @@ int main () {
     printf("code 0x%p..0x%p, data 0x%p..0x%p, " __DATE__ " " __TIME__ "\n",
             &app, _etext, _sdata, _ebss);
 
-    PinA<7> ledB;
-    ledB.mode(Pinmode::out);
-    
+    Timer<5,2> backlight;
+    backlight.init(1000, 1080); // 0..999 @ 108 MHz / 1080, i.e. 100 Hz
+    backlight.pwm(100);
+
+    PinH<11>::mode(Pinmode::alt_out, 2);
+
     while (true) {
         led(1);
-        ledB = 1;
         printf("<%d>\n", now());
         while (true)
             if (auto ch = getch(); ch >= 0)
@@ -23,7 +25,6 @@ int main () {
                 break;
         delay(100);
         led(0);
-        ledB = 0;
         delay(900);
     }
 }
